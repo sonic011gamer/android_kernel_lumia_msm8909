5e8bcca5e22ffc13967e87cad386eef4f8b1c656
Igor121 <chernokalsky89@gmail.com>
2024-03-08 22:04:29 +0300

    Revert "ANDROID: cpufreq: concurrent_active_time by pid"
    This reverts commit 3cffd3ce5f7b836d80f78125d2872dbde766c99c.

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2884,7 +2884,6 @@ static const struct pid_entry tgid_base_stuff[] = {
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
-	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
 #endif
 };
 
@@ -3269,7 +3268,6 @@ static const struct pid_entry tid_base_stuff[] = {
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
-	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
 #endif
 };
 
